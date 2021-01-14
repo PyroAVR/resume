f14dab3cceffea36dc8449b5e5dabad6dbdaed8a
Andy Meyer <ahm7197@rit.edu>
2021-01-14 16:59:24 -0500

    Complete overhaul, converted to tailwind.
    This should be a little easier for the bots to read...

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
